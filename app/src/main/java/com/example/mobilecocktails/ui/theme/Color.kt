1d89b83f887a4685ae9a09171bd3d0a3d384ea8b
TimotiPC <tymek9.pawlowski@gmail.com>
2025-04-15 18:25:21 +0200

    Dodano osbługe motywów ciemnych i jasnych

--- app/src/main/java/com/example/mobilecocktails/ui/theme/Color.kt
+++ app/src/main/java/com/example/mobilecocktails/ui/theme/Color.kt
@@ -1,11 +1,29 @@
 package com.example.mobilecocktails.ui.theme
 
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.material3.darkColorScheme
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val LimeGreen = Color(0xFF6CD47D)
+val OrangeSunset = Color(0xFFFF9F45)
+val CocktailBlue = Color(0xFF3A86FF)
+val Surface = Color(0xFF74B644)
+val Purple40 = CocktailBlue
+val PurpleGrey40 = LimeGreen
+val Pink40 = OrangeSunset
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+
+private val LightColorScheme = lightColorScheme(
+    primary = CocktailBlue,
+    secondary = LimeGreen,
+    tertiary = OrangeSunset,
+    surface= Surface
+)
+
+private val DarkColorScheme = darkColorScheme(
+    primary = CocktailBlue,
+    secondary = LimeGreen,
+    tertiary = OrangeSunset,
+    surface= Surface
+)
+
