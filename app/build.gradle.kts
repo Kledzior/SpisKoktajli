1330af35aaa9aa48e25660cb832dcf33858d1b19
Kledzior <bartek.kledzik@interia.pl>
2025-04-28 20:47:24 +0200

    Dodano droga aktywnosc

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,4 +62,8 @@ dependencies {
     implementation("androidx.navigation:navigation-compose:2.7.5")
     implementation("androidx.activity:activity-compose:1.8.2")
     implementation("io.coil-kt:coil-compose:2.4.0")
+    implementation ("androidx.compose.ui:ui:1.5.0")
+    implementation ("androidx.compose.material3:material3:1.0.0")
+    implementation ("androidx.compose.material:material-icons-core:1.5.0")
+    implementation ("androidx.compose.material:material-icons-extended:1.0.0")
 }
